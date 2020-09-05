97a6ae238a7f4a231f4eb9d8482b928ae828846a
Leandro Costa <leandrodavidcosta@gmail.com>
2020-09-05 19:19:35 -0300

    Rename Toast button to Greet + Add Count functionality

--- app/src/main/java/com/example/myfirstapp/FirstFragment.kt
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -24,8 +26,33 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Set click listener to the random button
         view.findViewById<Button>(R.id.random_button).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        // Set click listener to the greeting button
+        view.findViewById<Button>(R.id.greeting_button).setOnClickListener {
+            val myToast = Toast.makeText(context, getString(R.string.greeting_text), Toast.LENGTH_SHORT)
+            myToast.show()
+        }
+
+        // Set click listener to the count button
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
+        }
+    }
+
+    /**
+     * Basic method which counts starting from the number on the screen (Incrementing by one the
+     * number and then showing it on the screen)
+     * @param view The current view from which the starting number is extracted
+     */
+    private fun countMe(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString = showCountTextView.text.toString()
+        var count = countString.toInt()
+        count++
+        showCountTextView.text = count.toString()
     }
 }
