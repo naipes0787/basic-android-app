de66cb5b592e4d4ecb3551f0040520711dc15aba
Leandro Costa <leandrodavidcosta@gmail.com>
2020-09-05 20:24:04 -0300

    Change contact message

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "We don't support email contact yet", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
